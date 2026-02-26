d054b5b3da8ea36bd4b4ca73dd88cb5dfbbf4213
Truc QUynh <nguyenngoctrucquynh2005@gmail.com>
2026-02-27 00:45:59 +0700

    fix HomeController structure

--- src/main/java/vn/edu/hcmut/cse/adse/lab/controller/HomeController.java
+++ src/main/java/vn/edu/hcmut/cse/adse/lab/controller/HomeController.java
@@ -1,13 +1,19 @@
 package vn.edu.hcmut.cse.adse.lab.controller;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    // Handle root URL and show welcome page
+
+    @PostConstruct
+    public void init() {
+        System.out.println(">>> HomeController loaded <<<");
+    }
+
     @GetMapping("/")
     public String index() {
-        return "index"; // corresponds to index.html template
+        return "index";
     }
 }
